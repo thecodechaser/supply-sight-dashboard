124f0cc03285241e836e72c35ffa937866f46915
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2025-08-28 22:47:03 +0530

    Add server

---
+++ src/apollo/client.js
@@ -0,0 +1,20 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      errorPolicy: 'all',
+    },
+    query: {
+      errorPolicy: 'all',
+    },
+  },
+});
+
+export default client;
